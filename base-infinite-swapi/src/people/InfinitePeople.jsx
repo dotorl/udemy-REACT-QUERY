0d87744a0c1c90d890abe6b92d8ba7a03ef3f657
choks <choks0225@naver.com>
2023-01-12 18:56:57 +0900

    fix: section 3 complete

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,13 +1,30 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Person } from "./Person";
+import InfiniteScroll from 'react-infinite-scroller';
+import { useInfiniteQuery } from 'react-query';
+import { Person } from './Person';
 
-const initialUrl = "https://swapi.dev/api/people/";
+const initialUrl = 'https://swapi.dev/api/people/';
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfinitePeople() {
+  const { data, fetchNextPage, hasNextPage, isSuccess, isLoading, isError } = useInfiniteQuery(
+    'sw-poeple',
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    { getNextPageParam: (lastPage) => lastPage.next || undefined }
+  );
+
+  if (isLoading) return <p>Loading..</p>;
+  if (isError) return <p>error</p>;
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  return (
+    <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+      {data.pages.map((pageData, i) =>
+        pageData.results.map((person, index) => (
+          <Person key={person.name} name={person.name} hairColor={person.hair_color} eyeColor={person.eye_color} />
+        ))
+      )}
+    </InfiniteScroll>
+  );
 }
